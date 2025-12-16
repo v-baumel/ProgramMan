13dac1603380e907c0275d0f87b7b35228529261
v-baumel <vini.baumel@gmail.com>
2025-12-15 22:08:23 -0300

    pellet update

--- codigo/Enviroment.py
+++ codigo/Enviroment.py
@@ -13,7 +13,7 @@ class Wall(pygame.sprite.Sprite):
 class Pellet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface([TILE_SIZE, TILE_SIZE])
-        self.image.fill(C.BLACK)
+        self.image = pygame.Surface([TILE_SIZE//3, TILE_SIZE//3])
+        self.image.fill(C.BLUE)
         self.rect = self.image.get_rect()
         self.rect.center = (x + TILE_SIZE // 2, y + TILE_SIZE // 2)
